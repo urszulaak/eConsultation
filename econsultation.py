6f4930a4a4b16137f390be171ded6cf1e1377d63
unknown <mala.urszula@gmail.com>
2024-10-14 23:58:13 +0200

    MVC Home skeleton (without model)

--- econsultation.py
+++ econsultation.py
@@ -2,8 +2,8 @@ from core.Core import Core
 
 class Main:
     def run():
-            app = Core.openController("home")
-            app.main()
+        app = Core.openController("home")
+        app.main()
 
 if __name__ == '__main__':
     Main.run()
